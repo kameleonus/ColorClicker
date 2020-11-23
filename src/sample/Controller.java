679536ad3724a3998cc655c8eda7c19b9f471cff
kameleonus <kamil10.00@o2.pl>
2020-11-23 20:03:59 +0100

    Color changing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,14 +10,60 @@ public class Controller {
     @FXML
     public Label points;
     @FXML
-     Button a = new Button();
-    public int wynik;
+    Button a = new Button();
+    @FXML
+    public Label lookFor;
+
     Random random = new Random();
+    String cc="";
+
+    public int wynik;
+
     public void score(){
         wynik++;
         points.setText(String.valueOf(wynik));
+        RandomColor(random.nextInt(10));
+        a.setStyle("-fx-background-color: "+cc+"; -fx-background-radius: 30px;");
+        lookFor.setStyle("-fx-background-color: "+cc);
         a.setLayoutY(random.nextInt(550)+30);
         a.setLayoutX(random.nextInt(450)+30);
+    }
+
+    private void RandomColor(int pointerColor) {
+        switch (pointerColor) {
+            case 0:
+                cc = "PURPLE";
+                break;
+            case 1:
+                cc = "LIGHTBLUE";
+                break;
+            case 2:
+                cc = "YELLOW";
+                break;
+            case 3:
+                cc = "PINK";
+                break;
+            case 4:
+                cc ="ORANGE";
+                break;
+            case 5:
+                cc ="crimson";
+                break;
+            case 6:
+                cc ="VIOLET";
+                break;
+            case 7:
+                cc ="Brown";
+                break;
+            case 8:
+                cc ="DEEPSKYBLUE";
+                break;
+            case 9:
+                cc ="HONEYDEW";
+                break;
+            default:
+                cc="RED";
+        }
     }
     public void misclic(){
         if(wynik>0)
