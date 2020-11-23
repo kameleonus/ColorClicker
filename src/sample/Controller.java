42ddc8d22e718b4e8b2d1695d4710251bace4964
kameleonus <kamil10.00@o2.pl>
2020-11-23 12:57:45 +0100

    starts to work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+
+import java.util.Random;
+
 public class Controller {
+    @FXML
+    public Label points;
+    @FXML
+     Button a = new Button();
+    public int wynik;
+    Random random = new Random();
+    public void score(){
+        wynik++;
+        points.setText(String.valueOf(wynik));
+        a.setLayoutX(random.nextInt(450)+20);
+        a.setLayoutY(random.nextInt(350)+20);
+    }
 }
