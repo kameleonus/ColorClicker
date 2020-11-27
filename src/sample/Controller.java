b94cc55fbe38ffff03f174b421a7232543418b1f
kameleonus <kamil10.00@o2.pl>
2020-11-27 10:34:31 +0100

    2 types of clicker

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,17 +11,27 @@ public class Controller {
     @FXML
     public Label points;
     @FXML
+    public Label outOF;
+    @FXML
+    public Label Tpoints;
+    @FXML
     Button a = new Button();
+    @FXML
+    Button Ta = new Button();
     @FXML
     Button startButton = new Button();
     @FXML
+    Button TimedStartButton = new Button();
+    @FXML
     public Label lookFor;
+    @FXML
+    public Label TlookFor;
     @FXML
     public Label lives;
     @FXML
-    public Label endMessage;
+    public Pane pane;
     @FXML
-    public Pane tlo;
+    public Pane Tpane;
     @FXML
     public Pane startPane;
     @FXML
@@ -32,15 +42,32 @@ public class Controller {
     String cc="";
     public int wynik;
     long meantime;
-    public void start(){
+    long startTime;
+    public void Standard(){
         startButton.setDisable(true);
         startButton.setVisible(false);
+        TimedStartButton.setDisable(true);
+        TimedStartButton.setVisible(false);
         startPane.setVisible(false);
         startPane.setDisable(true);
-        tlo.setVisible(true);
+        pane.setVisible(true);
+        pane.setDisable(false);
         meantime=System.currentTimeMillis();
     }
+    public void Timed(){
+        startButton.setDisable(true);
+        startButton.setVisible(false);
+        TimedStartButton.setDisable(true);
+        TimedStartButton.setVisible(false);
+        startPane.setVisible(false);
+        startPane.setDisable(true);
+        Tpane.setVisible(true);
+        Tpane.setDisable(false);
+        meantime=System.currentTimeMillis();
+        startTime=System.currentTimeMillis()/1000;
+    }
 
+//basic version
     public void score(){
         czas+=System.currentTimeMillis()-meantime;
         wynik++;
@@ -48,12 +75,26 @@ public class Controller {
         RandomColor(random.nextInt(15));
         a.setStyle("-fx-background-color: "+cc+"; -fx-background-radius: 30px;");
         lookFor.setStyle("-fx-background-color: "+cc);
-        if(wynik>5){
-        createFalseButton(wynik);}
         a.setLayoutY(random.nextInt(550)+30);
         a.setLayoutX(random.nextInt(450)+30);
         meantime=System.currentTimeMillis();
     }
+
+     //timed version
+        public void Timedscore(){
+            czas+=System.currentTimeMillis()-meantime;
+            wynik++;
+            Tpoints.setText(String.valueOf(wynik));
+            RandomColor(random.nextInt(15));
+            Ta.setStyle("-fx-background-color: "+cc+"; -fx-background-radius: 30px;");
+            TlookFor.setStyle("-fx-background-color: "+cc);
+            Ta.setLayoutY(random.nextInt(550)+30);
+            Ta.setLayoutX(random.nextInt(450)+30);
+            meantime=System.currentTimeMillis();
+            if((System.currentTimeMillis()/1000)-startTime>=10){
+                message("OUT OF TIME");
+            }
+        }
 
     private void RandomColor(int pointerColor) {
         switch (pointerColor) {
@@ -110,19 +151,22 @@ public class Controller {
         live--;
         lives.setText("LIVES: "+live);
         if(live==0) {
-            message();
+            message("OUT OF LIVES");
         }
     }
-    public void message(){
-        tlo.setVisible(false);
-        endMessage.setDisable(false);
-    endMessage.setVisible(true);
+    public void message(String type){
+        pane.setVisible(false);
+        pane.setDisable(true);
+        Tpane.setVisible(false);
+        Tpane.setDisable(true);
+        outOF.setText(type);
+        outOF.setDisable(false);
+    outOF.setVisible(true);
     a.setDisable(true);
+    Ta.setDisable(true);
     time.setText("AVG time: "+czas/wynik+"\n score: "+wynik);
     time.setVisible(true);
 
     }
-    public void createFalseButton(int wynik) {
 
-    }
 }
