6805703c6c3ddcd99aad381f6b47e6b009aec541
kameleonus <kamil10.00@o2.pl>
2020-12-02 15:51:08 +0100

    3 types of clicker the MultiColor needs some fine tunning

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,4 @@
-module Color.Clicker {
+module Color_Clicker {
     requires javafx.fxml;
     requires javafx.controls;
     opens sample;
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Color_Clicker");
+        primaryStage.setTitle("Color Clicker");
         primaryStage.setScene(new Scene(root, 500, 600));
         primaryStage.show();
 
